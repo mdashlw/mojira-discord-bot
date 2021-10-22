eb5931f78141b6817ec82eff5a4e7e674c842c4d
dericksonmark <markdericksonacct@gmail.com>
2021-10-22 10:13:13 -0400

    Force request reopening
    Force requests to be reopened, disregarding number of requests by a user within the past 24 hours, when a volunteer reopens a request.

--- src/events/request/RequestReopenEventHandler.ts
+++ src/events/request/RequestReopenEventHandler.ts
@@ -50,6 +50,6 @@ export default class RequestReopenEventHandler implements EventHandler<'messageR
 			}
 		}
 
-		await this.requestEventHandler.onEvent( requestMessage );
+		await this.requestEventHandler.onEvent( requestMessage, true );
 	};
 }
